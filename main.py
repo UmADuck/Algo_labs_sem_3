a01068ecf4a55c3f5f87194a8d9d70a2fb0194b6
UmADuck <Dmytro.Korol.ir.2021@lpnu.ua>
2022-11-20 19:56:22 +0200

    'add_lab_4_code'

--- main.py
+++ main.py
@@ -1,82 +1,30 @@
-from collections import Counter
+def read_data(file_path):
+    file_open = open(file_path, "r")
+    data = file_open.readlines()
+    list_of_word = []
+    for word in data:
+        list_of_word.append(word.strip("\n"))
+    return list_of_word
 
 
-def get_the_one_loved_beer(answers) -> list:
-    one_liked_beers = []
-    for count, all_worker_answers in enumerate(answers):
-        counter = all_worker_answers.count('Y')
-        if counter == 1:
-            one_liked_beers.append(answers[count].index('Y') + 1)
-    return one_liked_beers
-
-
-def find_liked_beers_by_workers(answers, workers, beers):
-
-    # Creating array of workers with their liked beers
-
-    liked_beers_by_worker_index = []
-
-    # Creating array of likes beers
-
-    liked_beers = []
-
-    for i in range(workers):
-        worker_beers = []
-        for j in range(beers):
-            if answers[i][j] == "Y":
-                worker_beers.append((j + 1))
-                liked_beers.append(j + 1)
-        liked_beers_by_worker_index.append(worker_beers.copy())
-        worker_beers.clear()
-    return liked_beers_by_worker_index, liked_beers
-
-
-# Check weather lists have some similar elements on crossing
-def lists_have_same_values(list1, list2):
-    return True if set(list1) & set(list2) else False
-
-
-# Create new list of elements on crossing
-def find_crossing(list1, list2):
-    return list((Counter(list1) & Counter(list2)).elements())
-
-
-def get_final_list_of_beers(results, liked_by_index, liked_beers):
-    for elem in liked_by_index:
-        if lists_have_same_values(results, elem):
+def longest_string_chain(data_inputs):
+    inputs = sorted(data_inputs, key=len)
+    word_chains = {word: 1 for word in inputs}
+    print(word_chains)
+    for word in word_chains:
+        if len(word) == 1:
             continue
-        else:
-            crossing_beers = find_crossing(elem, liked_beers)
-            res = crossing_beers[0]
-            results.append(res)
-    return results
-
-
-def print_final_result(final_result):
-    f = open('output.txt', 'w')
-    f.write(str(len(final_result)))
-    print(str(len(final_result)))
+        for i in range(len(word)):
+            checking_word = word.replace(word[i], '')
+            if checking_word in word_chains:
+                word_chains[word] = max(word_chains[checking_word] + 1, word_chains[word])
+    print(word_chains)
+    result_word_chain = max(word_chains.values())
+    return result_word_chain
 
 
 if __name__ == '__main__':
-    # Getting data from file and giving it to our variables
-
-    file = open("Lab3\input.txt")
-    line = file.read()
-
-    workers = int(line.split('\n')[0].split(' ')[0])
-    print('Workers:', workers)
-
-    beers = int(line.split('\n')[0].split(' ')[1])
-    print('Beers:', beers)
-
-    answers = line.split('\n')[1].split(' ')
-    print('Y-N answers: ', answers)
-
-    results = get_the_one_loved_beer(answers)
-
-    liked_beers_by_worker_index, liked_beers = find_liked_beers_by_workers(answers, workers, beers)
-
-    get_final_list_of_beers(results, liked_beers_by_worker_index, liked_beers)
-
-    print_final_result(results)
+    data = read_data('wchain.in')
+    print(data)
+    result_of_max_chain = longest_string_chain(data)
+    print(result_of_max_chain)
